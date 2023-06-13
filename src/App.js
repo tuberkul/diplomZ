29ee4b37bdb7a4fb2ee9a7e0e48f27e7d54dd76a
tuberkul <52918187+tuberkul@users.noreply.github.com>
2023-06-13 22:07:22 +0300

    MenuPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
 
 import Popup from './components/PopupCart/PopupCart';
-
 import Footer from './components/Footer/Footer';
-
+import NavMenu from './components/NavMenu/NavMenu';
 
 import './App.css';
-import NavMenu from './components/NavMenu/NavMenu';
+
 
 function App() {
   return (
@@ -22,6 +22,7 @@ function App() {
       <Router>
           <Routes>
             <Route exact path='/' element={<Home />} />
+            <Route exact path='/menu' element={<Menu />} />
           </Routes>
       </Router>
       <Footer/>
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,9 +1,75 @@
 import '../styles/Menu/Menu.css'
+
+import MinMenuItem from '../components/MInMenuItem/MinMenuItem';
+
 function Menu() {
 
     return (
       <div className="Menu">
-        
+        <div className="box-menu background-tint">
+          <div className="u-relative">
+            <h1 className='white'>Доставка итальянской кухни от 30 минут во все районы города!</h1>
+            <p style={{color: "#f8f8f8", opacity: 0.7}}>Каждый день | 11:00 - 21:30</p>
+          </div>
+        </div>
+        <div>
+          <h2 className='black' style={{textAlign: 'left', paddingLeft: '30px'}}>Закуски:</h2>
+          <div className='minMenuItem'>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          </div>
+          
+        </div>
+        <div>
+          <h2 className='black' style={{textAlign: 'left', paddingLeft: '30px'}}>Салаты:</h2>
+          <div className='minMenuItem'>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          </div>
+          
+        </div>
+
+        <div>
+          <h2 className='black' style={{textAlign: 'left', paddingLeft: '30px'}}>Супы:</h2>
+          <div className='minMenuItem'>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          </div>
+          
+        </div>
+
+        <div>
+          <h2 className='black' style={{textAlign: 'left', paddingLeft: '30px'}}>Вторые блюда:</h2>
+          <div className='minMenuItem'>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          </div>
+          
+        </div>
+
+        <div>
+          <h2 className='black' style={{textAlign: 'left', paddingLeft: '30px'}}>Десерты:</h2>
+          <div className='minMenuItem'>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          </div>
+          
+        </div>
+
+        <div>
+          <h2 className='black' style={{textAlign: 'left', paddingLeft: '30px'}}>Напитки:</h2>
+          <div className='minMenuItem'>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          <MinMenuItem data = {{img: "https://thumb.tildacdn.com/tild6437-6233-4432-b561-326262656466/-/cover/312x362/center/center/-/format/webp/c.png", name: "Салат", price: "240"}}/>
+          </div>
+          
+        </div>
       </div>  
     );
   }
