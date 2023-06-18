7bf6b19a342d8db6975fdc3529b7acf537d9715c
tuberkul <52918187+tuberkul@users.noreply.github.com>
2023-06-18 19:12:09 +0300

    123

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -137,7 +137,6 @@ function Home() {
             <p style={{color: "#f8f8f8", opacity: 0.7}}>Вкус, который питает здоровье!</p>
           </div>
         </div>
-        <svg className='arrowDown scrollToMenu' role="presentation" style={{fill:"#ffffff"}} x="0px" y="0px" width="38.417px" height="30.592px" viewBox="0 0 38.417 18.592"><g><path d="M19.208,18.592c-0.241,0-0.483-0.087-0.673-0.261L0.327,1.74c-0.408-0.372-0.438-1.004-0.066-1.413c0.372-0.409,1.004-0.439,1.413-0.066L19.208,16.24L36.743,0.261c0.411-0.372,1.042-0.342,1.413,0.066c0.372,0.408,0.343,1.041-0.065,1.413L19.881,18.332C19.691,18.505,19.449,18.592,19.208,18.592z"></path></g></svg>
         <div className="minMenu" name="menu">
           <div className="minMenu_headline">
               <h2>Разделы нашего меню</h2>
